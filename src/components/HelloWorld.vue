e6f033b2f5c8c85794c88a9ee5e020de17832f28
hwy <2398577441@qq.com>
2023-10-28 09:25:41 +0800

    git-it

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,89 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <el-card class="box-card">
+    <el-input v-model="ip" placeholder="请输入ip"/>
+    <el-button type="primary" @click="submitip" size="large">提交ip</el-button>
+    <el-input disabled v-model="serverip"/>
+    <el-button type="primary" @click="searchip" size="large">查询ip</el-button>
+  </el-card>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script setup>
+import { ElMessage } from "element-plus";
+import { ref, computed } from "vue";
+import axios from 'axios'
+const ip = ref("");
+
+
+const serverip = ref('')
+
+function validateIP(ip) {
+  // IP 地址的正则表达式，包括了 IPv4 和 IPv6 的格式验证
+  const regex =
+    /^(?:(?:^|\.)(?:\d{1,2}|1\d{2}|2[0-4]\d|25[0-5])){4}$|^(?:(?:^|:)(?:[a-fA-F0-9]{1,4})){8}$/;
+
+  return regex.test(ip);
+}
+
+const submitip = async() => {
+  // 格式验证通过 进行提交 发送axios请求
+  if (validateIP(ip.value)) {
+    const result = await axios.post('//webapi/url路径',ip)
+    // 返回结果，根据状态分类
+    console.log(result) 
+    
+    /* 如果成功
+      ElMessage({
+      showClose: true,
+      message: '成功信息',
+      type: 'success',
+    })
+    */
+
+  /*
+    如果失败
+    ElMessage({
+    showClose: true,
+    message: '失败信息',
+    type: 'warning',
+  })
+  */  
+  } else {
+    ElMessage.error("ip格式错误");
   }
+};
+
+
+// ip查询  发送axios请求
+const searchip = async()=>{
+  const result = await axios.get('/webapi/url地址')
+  console.log(result)
+  /*
+    查询成功
+    serverip.value = 返回的ip
+
+    查询出错
+    ElMessage({
+    showClose: true,
+    message: '错误信息',
+    type: 'warning',
+  */
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.el-card {
+  width: 800px;
+  height: 600px;
+  margin: 20px auto;
+}
+.el-input {
+  height: 50px;
+  margin-top: 80px;
+}
+.el-button {
+  margin-top: 15px;
 }
 ul {
   list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/webapi': {
+        target: "ip地址",
+        changeOrigin: true
+      }
+    }
+  }
 })
